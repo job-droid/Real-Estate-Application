5f226aca47bbfa549038a44d36be47e52da76288
pickachu143 <mejeca9330@evvgo.com>
2024-03-08 11:36:35 +0300

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+// import Button from "@mui/material/Button";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-slate-300 shadow-lg ">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-blue-900">Proxima</span>
+            <span className="text-slate-400-500">Estate</span>
+          </h1>
+        </Link>
+        <form
+          action=""
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
+          <input
+            type="text"
+            placeholder="Search ..."
+            className="bg-transparent placeholder-slate-500 focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex justify-between gap-4 flex-wrap">
+          <Link to='/'>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to='/about'>
+            <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
+          </Link>
+          <Link to='/sign-in'>
+            <li className="sm:inline text-slate-700 hover:underline">Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-      <h1 className='text-green-700 font-semibold'>Home page</h1>
+      <h1 className='text-green-700 font-bold'>Home page</h1>
     </div>
   )
 }
